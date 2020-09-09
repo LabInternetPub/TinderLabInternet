0204736475b72444198bb396b0c9adbee7da1ffc
Josep Roure Alcobé <roure@tecnocampus.cat>
2020-09-09 11:16:43 +0200

    Handling exceptions added. Api should intercept all exceptions to return an answer. Expeptions should be generated at the correct level

--- src/main/java/cat/tecnocampus/tinder/application/exception/ProfileNotFound.java
+++ src/main/java/cat/tecnocampus/tinder/application/exception/ProfileNotFound.java
@@ -1,10 +1,5 @@
 package cat.tecnocampus.tinder.application.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-//TODO : Move responseStatus from here (create another exception in the api layer)
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Not found")
 public class ProfileNotFound extends RuntimeException{
     public ProfileNotFound(String email)  {
         super("User " + email + " does not exist");
