59ef8b7b885a86ffabca012e30c9b09f9dbcfb13
Josep Roure Alcobé <roure@tecnocampus.cat>
2020-10-27 23:43:25 +0100

    login and jwt token generation working. Need to validate with token

--- src/main/java/cat/tecnocampus/tinder/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/cat/tecnocampus/tinder/configuration/security/ApplicationSecurityConfig.java
@@ -1,51 +1,49 @@
 package cat.tecnocampus.tinder.configuration.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import cat.tecnocampus.tinder.configuration.security.jwt.JwtConfig;
+import cat.tecnocampus.tinder.configuration.security.jwt.JwtUsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.crypto.SecretKey;
 import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
-    private static final String USERS_QUERY = "select nickname, password, enabled from tinder_user where nickname = ?";
 
+    private static final String USERS_QUERY = "select nickname, password, enabled from tinder_user where nickname = ?";
     private static final String AUTHORITIES_QUERY = "select username, role from authorities where username = ?";
 
     private DataSource dataSource;
 
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, DataSource dataSource) {
+    private final JwtConfig jwtConfig;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, DataSource dataSource, JwtConfig jwtConfig) {
         this.passwordEncoder = passwordEncoder;
         this.dataSource = dataSource;
+        this.jwtConfig = jwtConfig;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager(), jwtConfig))
+
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/profiles/me/**", "/quotes/**").hasRole("USER")
                 .antMatchers("/profiles/**").hasRole("ADMIN")
                 .anyRequest()
-                .authenticated()
-
-                .and()
-                .httpBasic()
-
-                .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .authenticated();
     }
 
     //Configure authentication manager
