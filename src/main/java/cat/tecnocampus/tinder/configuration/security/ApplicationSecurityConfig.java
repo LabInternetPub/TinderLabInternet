ff20269adf9502f9652adfc37578d9d08bc9a054
Josep Roure Alcobé <roure@tecnocampus.cat>
2020-11-16 10:18:04 +0100

    minor change

--- src/main/java/cat/tecnocampus/tinder/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/cat/tecnocampus/tinder/configuration/security/ApplicationSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.crypto.SecretKey;
 import javax.sql.DataSource;
 
 @EnableWebSecurity
